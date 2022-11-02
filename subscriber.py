409b63d184297c38e0938718e931e18ac09d15bb
Diego-Segovia <diego.segovia348@gmail.com>
2022-11-01 23:47:53 -0400

    MQTT topic subscriber

---
+++ subscriber.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+import json
+from google.cloud import pubsub_v1
+
+load_dotenv()
+
+PROJECT_ID = os.getenv("PROJECT_ID")
+TOPIC_ID = os.getenv("TOPIC_ID")
+SUBSCRIPTION_NAME = os.getenv("SUBSCRIPTION_NAME")
+
+credentials_path = './smart_thermostat_private_key.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+subscription_name = f'projects/{PROJECT_ID}/subscriptions/{SUBSCRIPTION_NAME}'
+
+subscriber = pubsub_v1.SubscriberClient()
+
+
+def callback(message):
+    data = message.data
+    number = json.loads(data)
+    number = number.split(":")[1].split("}")[0].split('"')[1].split('"')[0]
+    print(f"Gooogle Cloud Number: {number}.")
+    message.ack()
+
+future = subscriber.subscribe(subscription_name, callback=callback)
+print(f'Listening on {subscription_name}')
+
+with subscriber:
+    try:
+        future.result()
+    except:
+        future.cancel()
+        future.result()
+    
+
